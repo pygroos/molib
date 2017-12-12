5dfc66a59de6663de5185a0d8bc734af0cbc1533
moext <niucunhui@gmail.com>
2017-12-12 09:09:56 +0000

    add static function

--- src/Helper.php
+++ src/Helper.php
@@ -2,7 +2,7 @@
 /**
  * 	This file is part of the moext/molib package.
  *
- *	(c)	Moext <dev@moext.org>
+ *	(c)	Niu Cun hui <niucunhui@gmail.com>
  * 
  *	For the full copyright and license information, please view the LICENSE
  * 	file that was distributed with this source code. 
@@ -189,7 +189,15 @@ class Helper
 	///		Calculate
 	///
 	//////////////////////////////////////////////////////////////////////
-	
+
+    //
+    //  Calculate the remaining seconds before the current time tomorrow morning
+    //
+    static function lastTomorrow()
+    {
+        return strtotime(date('Y-m-d',strtotime('+1 day'))) - time();
+    }
+
 	static function arrDepth( $array )
 	{
 		if ( ! is_array( $array ) )
